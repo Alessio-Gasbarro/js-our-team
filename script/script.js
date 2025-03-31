d9fd90d1bf5ac615a1ba21064a4a6dc25e53c468
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-03-31 19:41:32 +0200

    [DONE] Added 7th member + BONUS DONE

--- script/script.js
+++ script/script.js
@@ -1,40 +1,46 @@
 //CREAZIONE ARRAY OBJECTS PER LE CARDS IN ORDINE COME MOSTRATO NELLO SCREENSHOT
 const teamMembers = [
     {
-      name: "Marco Bianchi",
-      role: "Designer",
-      email: "marcobianchi@team.com",
-      img: "./assets/img/male1.png"
+        name: "Marco Bianchi",
+        role: "Designer",
+        email: "marcobianchi@team.com",
+        img: "./assets/img/male1.png"
     },
     {
-      name: "Laura Rossi",
-      role: "Front-end Developer",
-      email: "laurarossi@team.com",
-      img: "../assets/img/female1.png"
+        name: "Laura Rossi",
+        role: "Front-end Developer",
+        email: "laurarossi@team.com",
+        img: "../assets/img/female1.png"
     },
     {
-      name: "Giorgio Verdi",
-      role: "Back-end Developer",
-      email: "giorgioverdi@team.com",
-      img: "../assets/img/male2.png"
+        name: "Giorgio Verdi",
+        role: "Back-end Developer",
+        email: "giorgioverdi@team.com",
+        img: "../assets/img/male2.png"
     },
     {
-      name: "Marta Ipsum",
-      role: "SEO Specialist",
-      email: "martarossi@team.com",
-      img: "../assets/img/female2.png"
+        name: "Marta Ipsum",
+        role: "SEO Specialist",
+        email: "martarossi@team.com",
+        img: "../assets/img/female2.png"
     },
     {
-      name: "Roberto Lorem",
-      role: "SEO Specialist",
-      email: "robertolorem@team.com",
-      img: "../assets/img/male3.png"
+        name: "Roberto Lorem",
+        role: "SEO Specialist",
+        email: "robertolorem@team.com",
+        img: "../assets/img/male3.png"
     },
     {
-      name: "Daniela Amet",
-      role: "Analyst",
-      email: "danielaamet@team.com",
-      img: "../assets/img/female3.png"
+        name: "Daniela Amet",
+        role: "Analyst",
+        email: "danielaamet@team.com",
+        img: "../assets/img/female3.png"
+    },
+    {
+        name: "Gerry Scotti",
+        role: "TV Host",
+        email: "gerryscotti@team.com",
+        img: "../assets/img/gerry.png"
     }
   ];
 
